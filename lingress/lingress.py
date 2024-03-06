0a04b8514215d27d7b25d8112c88d0201dd51601
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-03-06 19:27:59 +0700

    Update lingress.py

--- lingress/lingress.py
+++ lingress/lingress.py
@@ -116,8 +116,8 @@ class lin_regression:
         dataset['Label'] = dataset['Label'].str.replace("/", "_")
         
         
-        self.label_a = str(dataset[dataset['label'] == 0]['name'].unique()[0])
-        self.label_b = str(dataset[dataset['label'] == 1]['name'].unique()[0])
+        self.label_a = str(dataset[dataset['Label'] == 0]['name'].unique()[0])
+        self.label_b = str(dataset[dataset['Label'] == 1]['name'].unique()[0])
         
         self.dataset = dataset # Assign the dataframe to the class attribute
         
